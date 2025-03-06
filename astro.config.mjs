2d0ec2178017b5367856b82f38525090e858b3ff
hamster88 <hamster88@users.noreply.github.com>
2025-03-06 08:45:03 +0800

    build: 切换到unocss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config'
+import UnoCSS from 'unocss/astro'
 
-// https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	}
-});
+	integrations: [
+		UnoCSS({
+			injectReset: true // or a path to the reset file
+		}),
+	],
+})
