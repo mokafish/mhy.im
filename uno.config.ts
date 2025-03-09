de7761a50759d3d5e03b35685dcee4b4f1ff62c4
mokafish <mokafish@users.noreply.github.com>
2025-03-09 09:05:25 +0800

    style: 对 .site-menu-item 的样式修改

--- uno.config.ts
+++ uno.config.ts
@@ -3,7 +3,8 @@ import {
     defineConfig,
     presetAttributify,
     presetTypography,
-    presetUno
+    presetUno,
+    extractorSplit
 } from 'unocss'
 
 export default defineConfig({
@@ -12,13 +13,17 @@ export default defineConfig({
             dark: {
                 dark: '.mdui-theme-dark', // 自定义暗黑选择器
                 light: '.mdui-theme-light' // 可选：自定义明亮模式选择器
-            }
+            },
+
         }),
 
         presetAttributify(), // required when using attributify mode
         presetUno(), // required
         presetTypography(),
     ],
+    blocklist:[
+        '[rounded=""]',
+    ]
 })
 
 
